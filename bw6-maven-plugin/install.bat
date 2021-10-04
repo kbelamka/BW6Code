b32da0a2d026149a4a33483fb7a9c5f44da43bbc
kbelamka <external.kaveree.belamkar@sky.de>
2021-10-05 01:04:35 +0530

    bw6-maven plugin.

--- bw6-maven-plugin/install.bat
+++ bw6-maven-plugin/install.bat
@@ -4,9 +4,9 @@ start cmd.exe /C mvn install:install-file -Dfile=com.tibco.bw.palette.shared_6.1
 
 start cmd.exe /C mvn install:install-file -Dfile=com.tibco.xml.cxf.common_1.3.200.001.jar -DgroupId=com.tibco.plugins -DartifactId=com.tibco.xml.cxf.common -Dversion=1.3.200 -Dpackaging=jar
 
-start cmd.exe /C mvn install:install-file -Dfile=bw6-maven-plugin-2.0.1.jar -DpomFile=pom.xml
+start cmd.exe /C mvn install:install-file -Dfile=bw6-maven-plugin-2.8.1.jar -DpomFile=pom.xml
 
-start cmd.exe /C mvn install:install-file -Dfile=fabric8/fabric8-maven-plugin-2.2.102.jar -DpomFile=fabric8/pom.xml
+start cmd.exe /C mvn install:install-file -Dfile=fabric8/fabric8-maven-plugin-4.4.1.jar -DpomFile=fabric8/pom.xml
 
 
 
